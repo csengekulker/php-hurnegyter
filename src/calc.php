2e052232022017e6594d9f9124f5a34ab8210dce
csengekulker <k71608883429@ktch.hu>
2022-05-24 09:08:52 +0200

    Calculation

--- src/calc.php
+++ src/calc.php
@@ -6,6 +6,28 @@ $page =  file_get_contents('templates/calc.html');
 
 //szamitas itt
 
+$a = $_GET['a'];
+$b = $_GET['b'];
+$c = $_GET['c'];
+$d = $_GET['d'];
+
+function calcArea ($a, $b, $c, $d) {
+    $s = ($a + $b + $c + $d) / 2;
+
+    $area = sqrt(
+        ($s - $a) *
+        ($s - $b) *
+        ($s - $c) *
+        ($s - $d)
+    );
+
+    return $area;
+}
+
+$area = calcArea($a, $b, $c, $d);
+
+$page = str_replace('{{ result }}', $area, $page);
+
 echo $page;
 echo file_get_contents('templates/foot.html');
 ?>
