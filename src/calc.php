32c194d86e96deeb24484636f848802a19e9d0f4
csengekulker <k71608883429@ktch.hu>
2022-05-24 08:56:36 +0200

    Form, navigation

--- src/calc.php
+++ src/calc.php
@@ -1,4 +1,11 @@
 <?php
 
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/nav.html');
+$page =  file_get_contents('templates/calc.html');
 
+//szamitas itt
+
+echo $page;
+echo file_get_contents('templates/foot.html');
 ?>
