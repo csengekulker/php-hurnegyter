d69613bdc6be338423e8c08b5ae3cd5e6ad40f4f
csengekulker <k71608883429@ktch.hu>
2022-05-24 09:51:45 +0200

    refactor & button

--- src/calc.php
+++ src/calc.php
@@ -6,11 +6,6 @@ $page =  file_get_contents('templates/calc.html');
 
 //szamitas itt
 
-$a = $_GET['a'];
-$b = $_GET['b'];
-$c = $_GET['c'];
-$d = $_GET['d'];
-
 function calcArea ($a, $b, $c, $d) {
     $s = ($a + $b + $c + $d) / 2;
 
@@ -23,10 +18,27 @@ function calcArea ($a, $b, $c, $d) {
 
     return $area;
 }
+// ha nem uresek
+if (
+    !empty($_GET['a']) and
+    !empty($_GET['b']) and
+    !empty($_GET['c']) and
+    !empty($_GET['d'])
+) {
+    $a = $_GET['a'];
+    $b = $_GET['b'];
+    $c = $_GET['c'];
+    $d = $_GET['d'];
 
-$area = calcArea($a, $b, $c, $d);
+    $area = calcArea($a, $b, $c, $d);
 
-$page = str_replace('{{ result }}', $area, $page);
+    $page = str_replace('{{ result }}', $area, $page);
+
+} else {
+    $area = "Hiba! Helytelen bemenő adatok vagy üres mezők!";
+
+    $page = str_replace('{{ result }}', $area, $page);
+}
 
 echo $page;
 echo file_get_contents('templates/foot.html');
