666a85f43409fd9ac6b7c4cdd6ac3c52c10a7179
csengekulker <k71608883429@ktch.hu>
2022-05-24 08:37:37 +0200

    Navbar

--- src/about.php
+++ src/about.php
@@ -1,4 +1,7 @@
 <?php
 
-
+echo file_get_contents('templates/head.html');
+echo file_get_contents('templates/nav.html');
+echo file_get_contents('templates/about.html');
+echo file_get_contents('templates/foot.html');
 ?>
